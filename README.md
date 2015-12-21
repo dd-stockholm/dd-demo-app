ca5a9763ca7ecb78adbb677067e926130fca8594
Björn Blomqvist <bjorn@bjornblomqvist.se>
2015-12-21 18:47:43 +0100

    Added static serving of angularjs/app.

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# dd-demo-app
+# DD Demo APP
+
+## Run locally
+
+    ./gradlew run
+
+Then go to the just opened port. *(see the log)*
+
+
--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -2,6 +2,7 @@ import static spark.Spark.*;
 
 public class HelloWorld {
     public static void main(String[] args) {
+        externalStaticFileLocation("./angularjs/app/");
         get("/hello", (req, res) -> "Hello World");
     }
 }
