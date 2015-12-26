ca18ff3f6eae34bd8f9ac3f4819840c247578a01
Roger Nordquist <roger.nordquist@gmail.com>
2015-12-26 15:58:13 +0100

    json isnt returned

--- src/main/java/org/dd/demoapp/hello/HelloResource.java
+++ src/main/java/org/dd/demoapp/hello/HelloResource.java
@@ -6,14 +6,11 @@ import org.slf4j.LoggerFactory;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
-import javax.ws.rs.core.MediaType;
 import java.util.Optional;
 
 @Service
 @Path("/hello")
-@Produces(MediaType.APPLICATION_JSON)
 public class HelloResource {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(HelloResource.class);
