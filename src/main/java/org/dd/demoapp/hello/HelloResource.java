e65816c9bccfb1383cad244ef27e941d8ac9eee8
Roger Nordquist <roger.nordquist@gmail.com>
2015-12-26 15:55:24 +0100

    fixed logger name

--- src/main/java/org/dd/demoapp/hello/HelloResource.java
+++ src/main/java/org/dd/demoapp/hello/HelloResource.java
@@ -1,11 +1,13 @@
 package org.dd.demoapp.hello;
 
-import org.dd.demoapp.App;
 import org.jvnet.hk2.annotations.Service;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.ws.rs.*;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import java.util.Optional;
 
@@ -14,7 +16,7 @@ import java.util.Optional;
 @Produces(MediaType.APPLICATION_JSON)
 public class HelloResource {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(App.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(HelloResource.class);
 
     @GET
     public String hello(@QueryParam("name") Optional<String> nameOpt) {
