71524ef89d7e8051e9366123dd744642dafb2167
Schlebbe <Sebastian@temtron.se>
2016-02-07 14:46:10 +0100

    Party individual pages

--- angularjs/app/app.js
+++ angularjs/app/app.js
@@ -18,6 +18,38 @@ var ddDemoApp = angular.module('dd-demo-app', [
       templateUrl: 'partials/about.html',
       controller: 'AboutController'
     }).
+    when('/delegation/m', {
+      templateUrl: 'partials/m.html',
+      controller: 'PartiesController'
+    }).
+    when('/delegation/kd', {
+      templateUrl: 'partials/kd.html',
+      controller: 'PartiesController'
+    }).
+    when('/delegation/v', {
+      templateUrl: 'partials/v.html',
+      controller: 'PartiesController'
+    }).
+    when('/delegation/sd', {
+      templateUrl: 'partials/sd.html',
+      controller: 'PartiesController'
+    }).
+    when('/delegation/s', {
+      templateUrl: 'partials/s.html',
+      controller: 'PartiesController'
+    }).
+    when('/delegation/mp', {
+      templateUrl: 'partials/mp.html',
+      controller: 'PartiesController'
+    }).
+    when('/delegation/l', {
+      templateUrl: 'partials/l.html',
+      controller: 'PartiesController'
+    }).
+    when('/delegation/c', {
+      templateUrl: 'partials/c.html',
+      controller: 'PartiesController'
+    }).
     otherwise({
       redirectTo: 'index.html'
     });
